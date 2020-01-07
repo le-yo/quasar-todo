9fe4dceb37f7db213ce7946b12273b95991c02fa
Le-Yo <le-yo@le-yo.com>
2020-01-07 21:46:58 +0300

    initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cupiditate dignissimos ducimus est magni
+      minus officia optio quibusdam suscipit totam velit veritatis vitae, voluptas? Accusantium aperiam dicta iusto
+      totam vel.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi aspernatur eius laudantium minima
+      nesciunt odio quaerat quam qui ratione, temporibus veniam. Culpa distinctio enim labore repellendus rerum
+      voluptas.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
